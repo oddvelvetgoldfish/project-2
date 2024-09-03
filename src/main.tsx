9bed27f63625d02d63637f95273c07dc938b3953
Sara Gharagozlou <oddvelvetgoldfish@gmail.com>
2024-09-02 21:31:28 -0700

    Set up Tailwind Set up prettier tailwind class sorting

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Start } from './start';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Start />
+  </StrictMode>
+);
---
+++ src/start.tsx
@@ -0,0 +1,3 @@
+export const Start = () => {
+  return <div className="h-[300px] w-[300px] bg-background">Start</div>;
+};
