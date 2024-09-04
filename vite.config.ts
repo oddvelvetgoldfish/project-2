333474ccf31738899ecd1065e8400faae983d140
Sara Gharagozlou <oddvelvetgoldfish@gmail.com>
2024-09-04 14:37:59 -0700

    Making sure README is copied to gh-pages branch

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "README.md",
+          dest: "",
+        },
+      ],
+    }),
+  ],
   base: "/project-2",
   build: {
     outDir: "dist",
