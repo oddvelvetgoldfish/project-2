8aed2aaf5f0bcbf0e0756d1bf8e1714d96658178
Sara Gharagozlou <oddvelvetgoldfish@gmail.com>
2024-09-04 13:59:22 -0700

    Renamed start.tsx to home.tsx Added title to app Removed absolute base in config

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { Start } from './start';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Start } from "./home";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Start />
-  </StrictMode>
+  </StrictMode>,
 );
