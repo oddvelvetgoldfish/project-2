4145c4e8eedd742cb15dc04fdb310265a8569e31
Sara Gharagozlou <oddvelvetgoldfish@gmail.com>
2024-09-04 13:26:51 -0700

    Added really basic content to About

--- src/about-page.tsx
+++ src/about-page.tsx
@@ -1,7 +1,12 @@
 export const AboutPage = () => {
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-1 flex-col gap-2">
       <h2 className="text-2xl">About</h2>
+      <div className="flex h-full flex-1 items-center justify-center pb-12">
+        <p className="text-center">
+          This is a simple GIPHY search app built with care by Sara Gharagozlou.
+        </p>
+      </div>
     </div>
   );
 };
