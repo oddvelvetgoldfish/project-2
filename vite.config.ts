11f2564d172bc6e278e0dd8cf023db0952020440
Sara Gharagozlou <oddvelvetgoldfish@gmail.com>
2024-09-04 14:34:36 -0700

    Update config to output build according to deliverable spec

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,18 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   base: "/project-2",
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        entryFileNames: "js/main.js",
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name && assetInfo.name.endsWith(".css")) {
+            return "css/style.css"; // Specify output name for CSS file
+          }
+          return "[name].[ext]"; // Default for other assets
+        },
+      },
+    },
+  },
 });
